73dcf3221d15ff9bc4324754680fdb663bdb105b
Luke <lukemiller58@yahoo.com>
2018-09-18 11:21:56 -0500

    added pwd cmd

--- bash.js
+++ bash.js
@@ -1,10 +1,12 @@
+// const path = require('path');
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
   // const writeThis = process.stdout.write;
   if (cmd === 'pwd') {
-    process.stdout.write(cmd)
+    process.stdout.write(process.cwd())
 
   }
   else {process.stdout.write('You typed: ' + cmd)}
